b7ccaed0aa6d676822d88403160ddbbf931c3b76
Akumonogatari <112125654+Akumonogatari@users.noreply.github.com>
2022-08-28 11:33:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,15 @@ import discord
 from discord.ext import commands
 from urllib.request import Request, urlopen
 import os
+from actualisation import *
 
 intent = discord.Intents().all()
 bot = commands.Bot(command_prefix= "*", description= "Bot for summoner's war rewards",intents=intent)
 
+
 @bot.event
 async def on_ready():
     print("Ready")
-
-def actualisation():
-    global nb_validCode
-    global dic
-
-    req = Request('https://swq.jp/_special/rest/Sw/Coupon?_csrf_token=0r_X6Mr_qpxIuYfbxaLx3M4BnPx_zeZ2PgcCMAD9QuuFTlvE9e-HbCTxt0SvYCvmvasDeS1Uea3NJq-bH769QRYfKCZ84Y4mWGHpztVtFULbDvBrS4Kr6qixxjiAvDqJs7DH85SJBh0&_ctx[b]=master&_ctx[c]=JPY&_ctx[l]=fr-FR&_ctx[t]=Europe%2FParis%3B%2B0200&results_per_page=50', headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(req,timeout=10).read()
-    web = str(webpage)
-    f= open("page3.json","w")
-    f.write(web)
-    f.close()
-
-    dic = {}
-
-    nb_validCode = web.count('verified')    #nombres de codes valides
-
-    debut = 0
-
-    for i in range(nb_validCode):
-        
-        lab= web.find('Label',debut)                  #recherche du nom du code
-        a= web.find('"',lab+8)              
-        name=""                              
-        for i in range(lab+8,a):
-            name += web[i]
-
-        rec=[]
-        start=a
-        while start < web.find("verified",a):   #recherche des récompenses
-            q = web.find('Quantity',start-100)     #recherche de la quantité
-            b = web.find('"',q+11)
-            quant=""                              
-            for i in range(q+11,b):
-                quant += web[i]
-            start = q
-
-            lab2 = web.find('"Label":',start)   #recherche du nom  
-            b = web.find('"',lab2+11)              
-            recName=""                              
-            for i in range(lab2+9,b):
-                recName += web[i]
-            start = web.find('Quantity',b)
-            rec.append((quant,recName))
-
-
-        debut =  web.find('Created',b)
-        dic[name] = rec
-        
 
 @bot.command()
 async def ping(ctx):
@@ -64,7 +18,8 @@ async def ping(ctx):
 
 @bot.command()
 async def code(ctx):
-    actualisation()
+    global nb_validCode,dic
+    nb_validCode,dic = actualisation()
     message = f"Il y a actuellement {nb_validCode} codes valides qui sont : \n"
     if nb_validCode == 1:
         message = f"Il y a actuellement {nb_validCode} code valide qui est : \n"
@@ -78,14 +33,36 @@ async def code(ctx):
         message = "Il n'y malheureusement pas de code valide pour le moment essaie plus tard..."
 
     await ctx.send(message)
-
-
+  
 @bot.command()
-async def actu(ctx):
-    actualisation()
-    await ctx.send("Le bot a actualisé sa liste. *code pour voir les codes coupons dispo !")
-        
-        
-actualisation()
+async def ncode(ctx):
+    global nb_validCode,dic
+    new_nb_validCode,new_dic = actualisation()
+    temp,temp_nb = new_dic.copy(),new_nb_validCode
+
+    for key in temp:
+        if key in dic :
+            new_dic.pop(key)
+            new_nb_validCode -=1
+
+    if new_nb_validCode == 0 :
+        message = "Il n'y pas de nouveaux codes valides pour le moment essaie plus tard... (*code pour voir la liste des codes disponibles)"
+    elif new_nb_validCode == 1:
+        for i in new_dic :
+            a = i
+            b= new_dic[i]
+        message = f"Il y a un nouveau code : {a} -> x {b[0]} {b[1]}, lien IOS : <http://withhive.me/313/{a}>"
+    else :
+        message = f"Il y a {new_nb_validCode} nouveaux codes valides qui sont : \n"        
+        for key in new_dic :
+            message += f"{key}  -> "
+            for rec in new_dic[key]:
+                message += f"x {rec[0]} {rec[1]}, "
+            message += f"lien IOS : <http://withhive.me/313/{key}>\n"
+
+    nb_validCode,dic = temp_nb,temp
+    await ctx.send(message)
+
+nb_validCode,dic = actualisation()
 bot.run(os.environ["TOKEN"])
 
