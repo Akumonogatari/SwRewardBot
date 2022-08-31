8b59e925505068f9b813898bf662fee4601cb987
Akumonogatari <112125654+Akumonogatari@users.noreply.github.com>
2022-08-31 18:04:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,79 @@
+from discord.ext import tasks
 import discord
 from discord.ext import commands
 from urllib.request import Request, urlopen
 import os
 from actualisation import *
+import time
+ 
 
-intent = discord.Intents().all()
-bot = commands.Bot(command_prefix= "*", description= "Bot for summoner's war rewards",intents=intent)
+class MyClient(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # an attribute we can access from our task
+        self.counter = 0
+
+    async def setup_hook(self) -> None:
+        # start the task to run in the background
+        self.ncode.start()
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    @tasks.loop(seconds=2)  # task runs every 60 seconds
+    async def ncode(self):
+        channel = self.get_channel(739595329434943630)  # channel ID goes here
+        global nb_validCode,dic
+        new_nb_validCode,new_dic = actualisation()
+        temp,temp_nb = new_dic.copy(),new_nb_validCode
+        for key in temp:
+            if key in dic :
+                new_dic.pop(key)
+                new_nb_validCode -=1
+
+        if new_nb_validCode == 1:
+            for i in new_dic :
+                a = i
+                b= new_dic[i]
+            message = f"Il y a un nouveau code : {a} -> "
+
+            for i in range(len(b)):
+                message += f"x {b[i][0]} {b[i][1]},"
+
+            message += f" lien IOS : <http://withhive.me/313/{a}>"
+            await channel.send(message)
+            
+        elif new_nb_validCode > 1:
+            message = f"Il y a {new_nb_validCode} nouveaux codes valides qui sont : \n"        
+            for key in new_dic :
+                message += f"{key}  -> "
+                for rec in new_dic[key]:
+                    message += f"x {rec[0]} {rec[1]}, "
+                message += f"lien IOS : <http://withhive.me/313/{key}>\n"
+            await channel.send(message)
+
+        nb_validCode,dic = temp_nb,temp
+        print("hello")
+
+    @ncode.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()  # wait until the bot logs in
 
 
-@bot.event
-async def on_ready():
-    print("Ready")
 
-@bot.command()
-async def ping(ctx):
+client = MyClient(command_prefix= "*", description= "Bot for summoner's war rewards using swq.jp",intents=discord.Intents.all())
+
+@client.command()
+async def ping(ctx):    
     await ctx.send("pong")
 
-@bot.command()
+@client.command()
+async def ncode(ctx):    
+    await ctx.send("La commande ncode n'existe plus le bot s'actualise tout seul maintenant !")
+    
+@client.command()
 async def code(ctx):
     global nb_validCode,dic
     nb_validCode,dic = actualisation()
@@ -33,42 +90,7 @@ async def code(ctx):
         message = "Il n'y malheureusement pas de code valide pour le moment essaie plus tard..."
 
     await ctx.send(message)
-  
-@bot.command()
-async def ncode(ctx):
-    global nb_validCode,dic
-    new_nb_validCode,new_dic = actualisation()
-    temp,temp_nb = new_dic.copy(),new_nb_validCode
-
-    for key in temp:
-        if key in dic :
-            new_dic.pop(key)
-            new_nb_validCode -=1
-
-    if new_nb_validCode == 0 :
-        message = "Il n'y pas de nouveaux codes valides pour le moment essaie plus tard... (*code pour voir la liste des codes disponibles)"
-    elif new_nb_validCode == 1:
-        for i in new_dic :
-            a = i
-            b= new_dic[i]
-        message = f"Il y a un nouveau code : {a} -> "
-
-        for i in range(len(b)):
-            message += f"x {b[i][0]} {b[i][1]},"
-
-        message += f" lien IOS : <http://withhive.me/313/{a}>"
-        await ctx.send(message)
-    else :
-        message = f"Il y a {new_nb_validCode} nouveaux codes valides qui sont : \n"        
-        for key in new_dic :
-            message += f"{key}  -> "
-            for rec in new_dic[key]:
-                message += f"x {rec[0]} {rec[1]}, "
-            message += f"lien IOS : <http://withhive.me/313/{key}>\n"
-
-    nb_validCode,dic = temp_nb,temp
-    await ctx.send(message)
+        
 
 nb_validCode,dic = actualisation()
-bot.run(os.environ["TOKEN"])
-
+client.run(os.environ["TOKEN"])
